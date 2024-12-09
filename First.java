0d400937a99fde969945efdcac44194c9ddc7166
Khamiar <khamiarmshimba4@gmail.com>
2024-12-09 09:25:48 +0300

    first add in folder

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class First{
+	public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in); 	
+	System.out.println("enter name: ");
+	String name = scanner.next();
+	System.out.println( name);
+	}
+}	
